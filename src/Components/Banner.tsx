427d1f0b6898332dd6ad09bbd7b3cc636c7c6a43
Minhas10 <minhasahamod@gmail.com>
2026-09-13 14:13:42 +0600

    NavBar add & create Banner section structure

---
+++ src/Components/Banner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Baner from '../assets/banner-stack.png'
+const Banner = () => {
+    return (
+        <div className="m-1.5 flex justify-between  container mx-auto items-center ">
+            <div >
+                <h1 className="text-4xl font-bold">Build Your Ideal</h1>
+                <h1 className="text-[56px] font-bold leading-[1.05] tracking-[-2px] block bg-gradient-to-r from-[#FF5722] via-[#E91E63] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</h1>
+                <p className="text-lg text-gray-600 mt-4">Explore frontend,backend,database,and tooling options,</p>
+                <p className="text-lg text-gray-600 ">compare them side by side, and put together the stack that fits your </p> 
+                <p className="text-lg text-gray-600 ">next project</p>
+                <div className="flex gap-4 mt-6">
+                    <button className="btn bg-amber-600 rounded-[10px]">Explore Technologies</button>
+                    <button className="btn bg-white rounded-[10px]">Learn More</button>
+                </div>
+            </div>
+            <div>
+                <img src={Baner} alt="Banner" className="w-[400px] mb-4" />
+            </div>
+
+            
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Logo from '../assets/logo-text.png'
+import '../App.css'
+const Nav = () => {
+    return (
+        <nav className="p-5">
+            <div className="container mx-auto flex  justify-between">
+                <img src={Logo} alt='Logo' />
+                <div>
+                    <ul className="flex  items-center gap-8 p-3">
+                        <li className="home">Home</li>
+                        <li><a href="#technologies" >Technologies</a></li>
+                        <li><a href="#projects" >Projects</a></li>
+                        <li><a href="#about" >About</a></li>
+                        <li><a href="#contact" >Contact</a></li>
+                    </ul>
+                </div>
+                <div className="flex gap-3">
+                    <button className="btn btn-ghost">Sign in</button>
+                    <button className="btn btn-secondary rounded-2xl">Sign up</button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
