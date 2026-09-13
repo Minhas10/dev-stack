550b336998c734178821196be3da0040fa9abf02
Minhas10 <minhasahamod@gmail.com>
2026-09-13 14:33:19 +0600

    Some unauthorized bug fix

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import Baner from '../assets/banner-stack.png'
 const Banner = () => {
     return (
         <div className="m-1.5 flex justify-between  container mx-auto items-center ">
             <div >
                 <h1 className="text-4xl font-bold">Build Your Ideal</h1>
-                <h1 className="text-[56px] font-bold leading-[1.05] tracking-[-2px] block bg-gradient-to-r from-[#FF5722] via-[#E91E63] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</h1>
+                <h1 className="text-[56px] font-bold leading-[1.05] tracking-[-2px] block bg-linear-to-r from-[rgb(255,87,34)] via-[#E91E63] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</h1>
                 <p className="text-lg text-gray-600 mt-4">Explore frontend,backend,database,and tooling options,</p>
                 <p className="text-lg text-gray-600 ">compare them side by side, and put together the stack that fits your </p> 
                 <p className="text-lg text-gray-600 ">next project</p>
@@ -15,7 +14,7 @@ const Banner = () => {
                 </div>
             </div>
             <div>
-                <img src={Baner} alt="Banner" className="w-[400px] mb-4" />
+                <img src={Baner} alt="Banner" className="w-100 mb-4" />
             </div>
 
             
--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Logo from '../assets/logo-text.png'
 import '../App.css'
 const Nav = () => {
